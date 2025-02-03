65f8a9265127552584e6ebab432b95392eba8bbc
nikhilraikar <nikhil.raikar@apl-landau.de>
2025-02-03 07:56:05 +0100

    Created a seperate prompt file

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from job_scrapper import scrape_jobs_data
 from rank import filter_jobs_with_ai
+from prompts import profile_summary_prompt
 
 if __name__ == "__main__":
 
-    resume_text = """
-    As an ISTQB® Certified Senior Quality Assurance Engineer with over 6+ years of experience, I excel in full
-    lifecycle testing, including white box, black box, and regression testing. I am proficient in Cypres
-    Selenium, Java, Python, and SQL, and adept at developing and executing test plans, automating tasks, and
-    identifying defects to ensure the delivery of high-quality software. My strong analytical skills, combined
-    with a collaborative approach, drive the successful delivery of products that consistently meet and exceed
-    customer expectations. 
-    """
     jobs = scrape_jobs_data()
-    
+
     if jobs:
         print("🚀 Running AI Job Search Agent with Groq...")
-        filtered_jobs = filter_jobs_with_ai(jobs, resume_text)
+        filtered_jobs = filter_jobs_with_ai(jobs, profile_summary_prompt)
     else:
         print("🚀 No News jobs found, all LLMS are paused")
---
+++ prompts/profile_summary_prompt.py
@@ -0,0 +1,8 @@
+profile_summary = """
+    I am an ISTQB® Certified Senior Quality Assurance Engineer with over 6+ years of experience, I excel in full
+    lifecycle testing, including white box, black box, and regression testing. I am proficient in Cypres
+    Selenium, Java, Python, and SQL, and adept at developing and executing test plans, automating tasks, and
+    identifying defects to ensure the delivery of high-quality software. My strong analytical skills, combined
+    with a collaborative approach, drive the successful delivery of products that consistently meet and exceed
+    customer expectations. 
+    """
