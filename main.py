ae488458da24e01fe632070e0a19f1e195b9f222
nikhilraikar <nikhil.raikar@apl-landau.de>
2025-02-06 12:02:39 +0100

    Disbaled stepstone scrapper

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from job_scrapper import scrape_jobs_data
 from rank import filter_jobs_with_ai
 from prompts import profile_summary_prompt
-from stepstone_scrapper import scrape_stepstone_jobs
+# from stepstone_scrapper import scrape_stepstone_jobs
 
 if __name__ == "__main__":
 
-    # jobs = scrape_jobs_data()
+    jobs = scrape_jobs_data()
     # stepstone_scrapper = scrape_stepstone_jobs(
     #     "qa-engineer", "Germany", max_results=10)
 
